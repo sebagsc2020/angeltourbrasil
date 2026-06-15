2566267201e32660e7769062a42a04b9f255b7c8
sebagsc2020 <sebagsc2020@gmail.com>
2026-06-14 23:46:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'angel-tour-app-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
+  '/teste2.html',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300;14..32,400;14..32,600;14..32,700&family=Poppins:wght@500;600;700;800&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
